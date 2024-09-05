b61360d75dda6e293d2ffe0568ead656c446e5f4
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-04 20:20:47 -0600

    Update Pokemon.py

--- Pokemon.py
+++ Pokemon.py
@@ -2,13 +2,16 @@ import time
 import numpy as np
 import sys
 
-def delay_print(s):
 
-  for c in s:
+
+def delay_print(s):
+    
+    for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.05)
-    
+
+
 class Pokemon:
     def __init__(self, name, types, moves, EVs, health='==================='):
         
@@ -18,9 +21,11 @@ class Pokemon:
         self.attack = EVs['ATTACK']
         self.defense = EVs['DEFENSE']
         self.health = health
-        self.bars = 20
+        self.bars = 20 
+
 
     def fight(self, Pokemon2):
+        
         print("-----POKEMONE BATTLE-----")
         print(f"\n{self.name}")
         print("TYPE/", self.types)
@@ -35,15 +40,17 @@ class Pokemon:
         print("LVL/", 3*(1+np.mean([Pokemon2.attack,Pokemon2.defense])))
 
         time.sleep(2)
-      version = ['Fire', 'Water', 'Grass']
+
+        
+        version = ['Fire', 'Water', 'Grass']
         for i,k in enumerate(version):
             if self.types == k:
-              
+                
                 if Pokemon2.types == k:
                     string_1_attack = '\nIts not very effective...'
                     string_2_attack = '\nIts not very effective...'
 
-               
+                
                 if Pokemon2.types == version[(i+1)%3]:
                     Pokemon2.attack *= 2
                     Pokemon2.defense *= 2
@@ -52,7 +59,7 @@ class Pokemon:
                     string_1_attack = '\nIts not very effective...'
                     string_2_attack = '\nIts super effective!'
 
-              
+               
                 if Pokemon2.types == version[(i+2)%3]:
                     self.attack *= 2
                     self.defense *= 2
@@ -61,8 +68,10 @@ class Pokemon:
                     string_1_attack = '\nIts super effective!'
                     string_2_attack = '\nIts not very effective...'
 
-          while (self.bars > 0) and (Pokemon2.bars > 0):
-            # Print the health of each pokemon
+
+      
+        while (self.bars > 0) and (Pokemon2.bars > 0):
+            
             print(f"\n{self.name}\t\tHLTH\t{self.health}")
             print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
 
@@ -74,11 +83,11 @@ class Pokemon:
             time.sleep(1)
             delay_print(string_1_attack)
 
-
+            
             Pokemon2.bars -= self.attack
             Pokemon2.health = ""
 
-            
+           
             for j in range(int(Pokemon2.bars+.1*Pokemon2.defense)):
                 Pokemon2.health += "="
 
@@ -87,11 +96,12 @@ class Pokemon:
             print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
             time.sleep(.5)
 
+            
             if Pokemon2.bars <= 0:
                 delay_print("\n..." + Pokemon2.name + ' fainted.')
                 break
 
- 
+            
 
             print(f"Go {Pokemon2.name}!")
             for i, x in enumerate(Pokemon2.moves):
@@ -101,11 +111,11 @@ class Pokemon:
             time.sleep(1)
             delay_print(string_2_attack)
 
-           
+            
             self.bars -= Pokemon2.attack
             self.health = ""
 
-           
+            
             for j in range(int(self.bars+.1*self.defense)):
                 self.health += "="
 
@@ -122,7 +132,12 @@ class Pokemon:
         money = np.random.choice(5000)
         delay_print(f"\nOpponent paid you ${money}.\n")
 
-f __name__ == '__main__':
+
+
+
+
+
+if __name__ == '__main__':
     
     Charizard = Pokemon('Charizard', 'Fire', ['Flamethrower', 'Fly', 'Blast Burn', 'Fire Punch'], {'ATTACK':12, 'DEFENSE': 8})
     Blastoise = Pokemon('Blastoise', 'Water', ['Water Gun', 'Bubblebeam', 'Hydro Pump', 'Surf'],{'ATTACK': 10, 'DEFENSE':10})
@@ -137,5 +152,4 @@ f __name__ == '__main__':
     Ivysaur = Pokemon('Ivysaur\t', 'Grass', ['Vine Wip', 'Razor Leaf', 'Bullet Seed', 'Leech Seed'],{'ATTACK':4, 'DEFENSE':6})
 
 
-    Charizard.fight(Blastoise)
-
+    Charizard.fight(Blastoise) 
