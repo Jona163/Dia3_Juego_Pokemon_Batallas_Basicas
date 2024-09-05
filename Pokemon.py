1006103e616275c8a8a94b50a2978d9e60251a78
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-04 20:13:46 -0600

    Pokemon.py

--- Pokemon.py
+++ Pokemon.py
@@ -8,3 +8,30 @@ def delay_print(s):
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.05)
+    
+class Pokemon:
+    def __init__(self, name, types, moves, EVs, health='==================='):
+        # save variables as attributes
+        self.name = name
+        self.types = types
+        self.moves = moves
+        self.attack = EVs['ATTACK']
+        self.defense = EVs['DEFENSE']
+        self.health = health
+        self.bars = 20
+
+    def fight(self, Pokemon2):
+        print("-----POKEMONE BATTLE-----")
+        print(f"\n{self.name}")
+        print("TYPE/", self.types)
+        print("ATTACK/", self.attack)
+        print("DEFENSE/", self.defense)
+        print("LVL/", 3*(1+np.mean([self.attack,self.defense])))
+        print("\nVS")
+        print(f"\n{Pokemon2.name}")
+        print("TYPE/", Pokemon2.types)
+        print("ATTACK/", Pokemon2.attack)
+        print("DEFENSE/", Pokemon2.defense)
+        print("LVL/", 3*(1+np.mean([Pokemon2.attack,Pokemon2.defense])))
+
+        time.sleep(2)
